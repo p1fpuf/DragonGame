fbc67b75aa0536ef98ce3f60a33d95c84f1e3c7d
p1fpuf <ivan.cherkasov2013@yandex.ru>
2023-10-13 16:41:03 +0500

    EnergyShield
    Сделали скрипт файл в котором будет основная механика.

--- DragonGame/Assets/Scripts/DragonEgg.cs
+++ DragonGame/Assets/Scripts/DragonEgg.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class DragonEgg : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static float bottomY = -30f;
+
     void Start()
     {
         
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        var em = ps.emission;
+        em.enabled = true;
+
+        Renderer rend;
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
